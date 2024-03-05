1d63b6ed7dc08dbd9a29e8d963e8ea0f68d56ce6
jloffredo2 <joseph.r.loffredo@gmail.com>
2024-03-04 22:20:15 -0500

    remove estimate files

--- scripts/automation.R
+++ scripts/automation.R
@@ -1,23 +1,28 @@
+##################################################
+## Project: CBS ENR Super Tuesday 2024
+## Script purpose: Automate tar_make()
+## Date: March 2024
+## Author:
+##################################################
+
 library(targets)
 library(tarchetypes)
 library(lubridate)
 
-# schedule_script.R
-# This R script will run tar_make every 4 minutes, commit to git.
-
-# Path to the R script you want to run
-script_to_run <- "scripts/media_export_cleaner.R"
-
 # Function to execute the R script
 execute <- function() {
+  message(sprintf("%s: Running tar_make", format(Sys.time(), format="%Y-%m-%d %H:%M:%S")))
   # Use the system function to execute the R scrip
-  system(paste("Rscript", script_to_run))
+  tar_make()
 }
 
 # Main loop to schedule and execute the script every 4 minutes
 while (TRUE) {
-  message(sprintf("\n\nexecuting script: %s\n\n",format(Sys.time(), format="%Y-%m-%d %H:%M:%S")))
   execute_script()
-  message(sprintf("\n\nscript finished, waiting 4 minutes: %s\n\n",format(Sys.time(), format="%Y-%m-%d %H:%M:%S")))
-  Sys.sleep(240)  # Sleep for 4 minutes (240 seconds)
+
+  # push and commit to git
+  git2r::add(path = "*")
+  git2r::commit(message = "latest change")
+  
+  Sys.sleep(900)  # Sleep for 15 minutes (900 seconds)
 }
