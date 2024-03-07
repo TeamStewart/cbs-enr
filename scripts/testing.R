d25ecc1fbe15b1a9b53894d46b04cc62a7811ee1
Mason Reece <mpreece@mit.edu>
2024-03-07 08:59:31 -0500

    Update testing.R

--- scripts/testing.R
+++ scripts/testing.R
@@ -3,137 +3,80 @@ gc()
 
 library(tidyverse)
 library(targets)
+library(httr2)
+library(data.table)
+library(xml2)
 
-data <- tar_read(clean_NC_ALL_primary) |> 
-  filter(str_detect(race_name, regex("Governor|President", ignore_case=TRUE))) |> 
-  mutate(race_name = str_remove_all(race_name, " DEM$| REP$|-DEM$|-REP$|-LIB$| LIB$|-Democrat|-Republican|-Libertarian")) |> 
-  filter(candidate_party %in% c("Democrat", "Republican")) |> 
-  mutate(
-    jCde = case_when(
-      str_detect(race_name, "^US House") ~ str_extract(race_name, "\\d+") |> as.numeric() |> as.character(),
-      .default = "0"
-    ),
-    race_name = case_when(
-      str_detect(race_name, "^US House") ~ "House",
-      race_name == "NC SECRETARY OF STATE" ~ "Secretary Of State",
-      race_name == "NC ATTORNEY GENERAL" ~ "Attorney General",
-      race_name == "NC LIEUTENANT GOVERNOR" ~ "Lt Governor",
-      str_detect(race_name, "^Governor") ~ "Governor",
-      .default = race_name
-    )
-  ) |> 
-  rename(
-    office = race_name
-  ) |> 
-  mutate(eDate = "2024-03-05",
-         real_precinct = ifelse(virtual_precinct, "N", "Y")) |> 
-  left_join(lookup_geo, join_by(state == postalCode, jurisdiction == county_name))
+version <- request("https://results.enr.clarityelections.com/GA/113667/current_ver.txt") |> 
+  req_headers("Accept" = "application/txt") |> 
+  req_user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0") |> 
+  req_perform() |> 
+  resp_body_string() 
 
-lookup_cands <- read_csv("data/input/cbs_lookups/2024-03-01 Primary Candidates.csv",
-                  col_types = cols(.default = col_character()),
-                  col_names = c("state", "office", "ofc", "election", "etype", "jCde", "candidate_name", "candidate_lastname", "cId")) |>
-  filter(state == "North Carolina") |>
-  mutate(candidate_party = case_match(
-    etype,
-    "D" ~ "Democrat",
-    "R" ~ "Republican"))
+counties <- request(sprintf("https://results.enr.clarityelections.com/GA/113667/%s/json/en/electionsettings.json", version)) |> 
+  req_headers("Accept" = "application/json") |> 
+  req_user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0") |> 
+  req_perform() |> 
+  resp_body_json() |>
+  pluck("settings", "electiondetails", "participatingcounties") |> 
+  as_tibble_col() |> 
+  unnest(cols = value) |> 
+  separate_wider_delim(cols = value, delim = "|", names = c("county", "sitenum", "version", "timestamp", "unknown")) |> 
+  mutate(url = sprintf("https://results.enr.clarityelections.com/GA/%s/%s/%s/reports/detailxml.zip", county, sitenum, version))
 
-lookup_geo <- read_csv("data/input/cbs_lookups/All States and Counties.csv",
-                       skip = 1,
-                       col_names = c("state_name", "postalCode", "st", "state_fips", "county_name", "cnty", "county_fips")) |> 
-  select(-state_fips) |> 
-  mutate(county_name = str_to_upper(county_name))
+county <- "Appling"
 
-match <- data |>
-  distinct(office, jCde, candidate_party, candidate_name) |>
-  left_join(lookup_cands, join_by(office, jCde, candidate_party, candidate_name))
+request(counties$url[1]) |> 
+  req_headers("Accept" = "application/zip") |> 
+  req_user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0") |> 
+  req_perform(path = sprintf("data/raw/ga_%s.zip", county))
 
-lookup_cands_remaining <- lookup_cands |>
-  anti_join(filter(match, !is.na(state)), join_by(candidate_name))
+d <- read_xml("data/raw/detail.xml") |> 
+  as_list() |> 
+  pluck("ElectionResult", function(x) x[names(x) == "Contest"]) |> 
+  as_tibble_col()
 
-match2 <- match |>
-  filter(is.na(state)) |>
-  select(office, jCde, candidate_party, candidate_name) |>
-  mutate(candidate_lastname = str_remove_all(candidate_name, "Jr.*?$|III$|,") |> str_squish()) |>
-  mutate(candidate_lastname = str_extract(candidate_lastname, "\\s(\\w+)$") |> str_squish()) |>
-  # select(-candidate_name) |>
-  left_join(select(lookup_cands_remaining, -candidate_name), join_by(office, jCde, candidate_party, candidate_lastname))
+d |> 
+  unnest_longer(value) |> 
+  unnest_longer(col = value, names_repair = "unique") |> 
+  unnest_longer(col = value)
+  
+drop_cols <- "Precinct Summary|Electionware Copyright|Summary Results|Joint Primary Election|1-Mar-22|Total Votes Cast|Statistics|Registered Voters|Ballots Cast|Voter Turnout|March 1, 2022"
 
-match |>
-  filter(!is.na(state)) |>
-  bind_rows(match2) |>
-  rename(eType = etype) |> 
-  write_csv("data/input/cbs_lookups/primary_cands.csv")
-
-lookup_cands <- read_csv("data/input/cbs_lookups/primary_cands_nc.csv", 
-                         col_types = cols(.default = col_character())) |> 
-  select(-state, -candidate_lastname)
-
-
-# jType
-# key
-
-modified <- data |> 
-  filter(candidate_party %in% c("Democrat", "Republican")) |> 
-  mutate(
-    jCde = case_when(
-      str_detect(race_name, "^US House") ~ str_extract(race_name, "\\d+") |> as.numeric() |> as.character(),
-      .default = "0"
-    ),
-    race_name = case_when(
-      str_detect(race_name, "^US House") ~ "House",
-      race_name == "NC SECRETARY OF STATE" ~ "Secretary Of State",
-      race_name == "NC ATTORNEY GENERAL" ~ "Attorney General",
-      race_name == "NC LIEUTENANT GOVERNOR" ~ "Lt Governor",
-      str_detect(race_name, "^Governor") ~ "Governor",
-      .default = race_name
-    )
-  ) |> 
-  rename(
-    office = race_name
-  ) |> 
-  mutate(eDate = "2024-03-05",
-         real_precinct = ifelse(virtual_precinct, "N", "Y"),
-         etype = "P") |> 
-  left_join(lookup_geo, join_by(jurisdiction == county_name)) |> 
-  left_join(lookup_cands, join_by(office, jCde, candidate_party, candidate_name)) |> 
-  mutate(jType = ifelse(jCde == "0", "SW", "CD")) |> 
-  mutate(precinct_id = str_c(county_fips, precinct_id, sep = "_")) |> 
-  rename(pcntName = precinct_id) |> 
-  mutate(pcnt = pcntName,
-         pcntUUID = pcntName)
-
-
-
-example <- tibble(nc = jsonlite::read_json("~/Downloads/nc_results_2022.json")) |> 
-  unnest_wider(col = nc)
-
-example |> 
-  distinct(jType)
-
-
-final <- modified |> 
-  pivot_wider(names_from = vote_mode, values_from = precinct_total) |> 
-  rename(edayVote = `Election Day`,
-         earlyInPersonVote = `Early Voting`,
-         earlyByMailVote = `Absentee/Mail`,
-         provisionalVote = Provisional,
-         cName = candidate_name) |> 
-  mutate(cVote = edayVote + earlyInPersonVote + earlyByMailVote + provisionalVote,
-         ts = tar_read(time_NC_ALL_primary)) |> 
-  select(eDate, jType, real_precinct, st, etype, jCde, ofc, cnty, pcnt, pcntUUID, pcntName, 
-         cId, cName, cVote, edayVote, earlyInPersonVote, earlyByMailVote, provisionalVote,
-         ts) |> 
-  nest(candidates = c(cId, cName, cVote, edayVote, earlyInPersonVote, earlyByMailVote, provisionalVote)) |> 
-  # mutate(key = list(eDate, st, etype, jType, jCde, ofc, cnty, pcnt, pcntUUID))
-  nest(key = c(eDate, st, etype, jType, jCde, ofc, cnty, pcnt, pcntUUID)) |>
-  mutate(key2 = key) |>
-  unnest(cols = key2) |> 
-  mutate(key = map(key, as.list)) |> 
-  # mutate(key = map(key, pull)) |> 
-  select(eDate, jType, real_precinct, key, pcntName, candidates, jCde, ts, st, ofc, etype, pcntUUID, cnty)
-
-tar_read(cbs_NC_ALL_primary) |> 
-  filter(pcntName == "37001_01") |> 
-  select(key, candidates) |> 
-  unnest(cols = candidates)
+d <- fread(path, 
+           select = c(1, 2, 7, 9, 12),
+           skip = 3,
+           fill = TRUE,
+           blank.lines.skip = TRUE,
+           colClasses = list(character=1:12),
+           na.strings = "",
+           col.names = c("candidate_name", "race_name", "Election Day", "Absentee/Mail", "Early Voting")) |> 
+  as_tibble() |> 
+  mutate(race_name = na_if(race_name, "Vote For 1")) |> 
+  fill(race_name, .direction = "down") |> 
+  filter(!(str_detect(candidate_name, drop_cols))) |> 
+  mutate(precinct_id = str_extract(candidate_name, "\\d{4}")) |> 
+  fill(precinct_id, .direction = "down") |> 
+  filter(candidate_name != precinct_id) |> 
+  mutate(candidate_party = str_extract(race_name, ".*?\\s") |> str_squish(),
+         race_name = str_remove(race_name, candidate_party) |> str_squish(),
+         candidate_party = case_match(candidate_party,
+                                      "DEM" ~ "Democrat",
+                                      "REP" ~ "Republican"),
+         jurisdiction = county,
+         state = state) |> 
+  pivot_longer(cols = c(`Election Day`, `Absentee/Mail`, `Early Voting`), values_to = "precinct_total", names_to = "vote_mode") |> 
+  # mutate(race_name = case_when(
+  #   str_detect(race_name, "United States President") ~ "President",
+  #   str_detect(race_name, "United States Senator") ~ "US Senate",
+  #   str_detect(race_name, "United States Representative") ~ "US House",
+  #   str_detect(race_name, "Governor") ~ "Governor",
+  #   str_detect(race_name, "State Representative") ~ "State Legislature-Lower District",
+  #   str_detect(race_name, "State Senator") ~ "State Legislature-Upper District",
+  #   .default = race_name
+  # )) |> 
+  mutate(precinct_total = as.numeric(precinct_total),
+         precinct_total = replace_na(precinct_total, 0)) |> 
+  mutate(virtual_precinct = FALSE) |> 
+  filter(max(precinct_total) > 0, .by = c(race_name, candidate_name, candidate_party)) |> 
+  select(state, jurisdiction, precinct_id, race_name, candidate_name, candidate_party, vote_mode, precinct_total, virtual_precinct)
