50d32e6ebf35b1f6ce53dd41ceebee077d0f52ee
Mason Reece <mpreece@mit.edu>
2024-10-30 22:08:25 -0400

    move global paths to globals.R

--- scripts/util/globals.R
+++ scripts/util/globals.R
@@ -1,4 +1,17 @@
-PATH_GDRIVE = 
+PATH_GDRIVE = ""
 PATH_CBS_S3 = "cbsn-elections-external-models"
-PATH_LOCAL = 
+PATH_LOCAL = ""
 ELECTION_DATE = "2024_general"
+PATH_DROPBOX = ""
+
+local({
+  
+  user = Sys.info()["user"]
+  
+  if (user == "josephloffredo"){
+    PATH_DROPBOX <<- "~/Dropbox (MIT)/2024 Election Results"
+  } else if (user == "mason") {
+    PATH_DROPBOX <<- "~/Dropbox (MIT)/Research/2024 Election Results"
+  }
+  
+})
