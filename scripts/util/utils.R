7a989de5f9e46d2b5c8dab060260dbeb28c2213e
Mason Reece <mpreece@mit.edu>
2024-11-02 21:46:34 -0400

    let `get_gsheet` define col types

--- scripts/util/utils.R
+++ scripts/util/utils.R
@@ -3,9 +3,10 @@ lookup_state_name <- function(abbreviation) {
   state.name[match(abbreviation, state.abb)]
 }
 
-get_gsheet <- function(sheet) {
+get_gsheet <- function(sheet, ...) {
   googlesheets4::read_sheet(
     ss = "https://docs.google.com/spreadsheets/d/10MtdPvXsqA4ddjhrXgHA2TEYH5AtW1-dmvS1Bv7TgEk/edit?gid=0#gid=0",
-    sheet = sheet
+    sheet = sheet,
+    ...
   )
 }
