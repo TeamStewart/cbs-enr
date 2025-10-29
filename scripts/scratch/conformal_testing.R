9c30fbcc34388a573327ac7ff8f082cf22fb8e6d
Mason Reece <mpreece@mit.edu>
2025-10-29 14:24:44 -0400

    Update conformal_testing.R

--- scripts/scratch/conformal_testing.R
+++ scripts/scratch/conformal_testing.R
@@ -29,29 +29,25 @@ truth = read_csv(tests[length(tests)]) |>
     )
   )
 
-# models with `vote_mode` separate outcomes
 models = expand_grid(
-  method = "lm",
-  uncertainty = c("", "conformal"),
-  subset = c('vote_mode == "Early Voting"', 'vote_mode == "Absentee/Mail"', 'vote_mode == "Election Day"', ''),
-  outcome = expand_grid(party = c("dem", "rep")) |>
-    mutate(
-      o = glue({
-        "votes_governor_25_{party}"
-      })
-    ) |>
-    pull(o),
-  covariates = list(NULL, c(covars))
+  method = "quantreg",
+  uncertainty = "conformal",
+  subset = c(''),
+  outcome = c("turnout", "votes_governor_25_dem", "votes_governor_25_rep"),
+  c1 = list(NULL, c(covars))
 ) |>
   rowwise() |>
   mutate(
-    covar = str_replace(outcome, "governor_25", "potus_24"),
-    covars = list(c(covar, covariates))
+    c2 = case_when(
+      outcome == "votes_governor_25_dem" ~ "votes_potus_24_dem",
+      outcome == "votes_governor_25_rep" ~ "votes_potus_24_rep",
+      outcome == "turnout" ~ "votes_precFinal_21",
+      .default = NA_character_
+    ),
+    covars = list(c(c2, c1))
   ) |>
-  expand_grid(
-    paths = tests
-  ) |>
-  select(-covar, -covariates) |>
+  expand_grid(paths = tests) |>
+  select(-c1, -c2) |>
   mutate(
     out = pmap(
       list(paths, method, uncertainty, outcome, covars, subset),
@@ -67,12 +63,32 @@ models = expand_grid(
           uncertainty = uncertainty,
           outcome = outcome,
           subset = subset,
-          obs_cutoff = 0.1,
+          obs_cutoff = 0.25,
           weight_var = "reg24"
         )
       }
     )
   )
+
+t = models |>
+  mutate(
+    train = map(out, "train"),
+    valid = map(out, "valid"),
+    test = map(out, "test"),
+    out = map(out, "out")
+  ) |>
+  filter(
+    outcome == "votes_governor_25_dem",
+    paths == tests[5],
+    subset == "vote_mode == \"Early Voting\"",
+    uncertainty == "",
+    lengths(covars) == 1
+  )
+
+t |> pluck("train", 1, "votes_governor_25_dem") |> sum()
+t |> pluck("test", 1, "votes_governor_25_dem") |> sum()
+t |> pluck("valid", 1, "votes_governor_25_dem") |> sum()
+t |> pluck("out", 1, "estimate") |> sum()
 
 summaries = models |>
   mutate(
@@ -81,104 +97,22 @@ summaries = models |>
   unnest(cols = out) |>
   summarize(
     across(matches("^(estimate|lower|upper)$"), sum),
-    .by = c(method, uncertainty, subset, outcome, vote_mode, covars, timestamp)
+    .by = c(method, uncertainty, subset, outcome, covars, vote_mode, timestamp)
   )
 
-make_plot_voteShare_byMode <- function(d) {
-  d |>
-    filter(outcome != "turnout", uncertainty != "conformal") |>
-    mutate(
-      co = ifelse(lengths(covars) > 1, "with covariates", "without covariates")
-    ) |>
-    ggplot(aes(x = timestamp, y = estimate, color = outcome, shape = co)) +
-    geom_point(position = position_dodge(width = 0.1)) +
-    geom_line() +
-    geom_hline(
-      data = truth,
-      aes(yintercept = truth, color = outcome),
-      linetype = "dashed"
-    ) +
-    facet_wrap(~vote_mode) +
-    # geom_hline(yintercept = 1928516, linetype = "dashed", color = "#3791FF") +
-    # geom_hline(yintercept = 1251256, linetype = "dashed", color = "#F6573E") +
-    # geom_hline(yintercept = 3179772, linetype = "dashed", color = "black") +
-    labs(
-      x = "Time (EST)",
-      y = "Votes",
-      color = "Party",
-      shape = ""
-    ) +
-    theme_bw() +
-    scale_y_continuous(
-      labels = scales::label_comma(),
-      n.breaks = 7
-    ) +
-    scale_x_datetime(
-      date_breaks = "2 hour",
-      labels = scales::label_date(format = "%m-%d \n %I:%M %p", tz = "US/Eastern")
-    ) +
-    scale_color_manual(
-      values = c("votes_governor_25_dem" = "#3791FF", "votes_governor_25_rep" = "#F6573E"),
-      labels = c("votes_governor_25_dem" = "Democrat", "votes_governor_25_rep" = "Republican")
-    )
-}
 
-make_plot_voteShare_byMode(filter(summaries, subset == '')) # combined `vote_mode`
-make_plot_voteShare_byMode(filter(summaries, subset != '')) # separate `vote_mode`
-
-# total vote share plots, summed across modes
 summaries |>
-  filter(outcome != "turnout", uncertainty != "conformal", lengths(covars) == 1, subset != '') |>
-  summarize(
-    across(matches("^(estimate|lower|upper)$"), sum),
-    .by = c(outcome, timestamp)
-  ) |>
-  ggplot(aes(x = timestamp, y = estimate, color = outcome)) +
-  geom_point(position = position_dodge(width = 0.1)) +
-  geom_line() +
-  geom_hline(
-    data = summarize(truth, truth = sum(truth), .by = outcome),
-    aes(yintercept = truth, color = outcome),
-    linetype = "dashed"
-  ) +
-  labs(
-    x = "Time (EST)",
-    y = "Votes",
-    color = "Party"
-  ) +
-  theme_bw() +
-  scale_y_continuous(
-    labels = scales::label_comma(),
-    n.breaks = 7
-  ) +
-  scale_x_datetime(
-    date_breaks = "2 hour",
-    labels = scales::label_date(format = "%m-%d \n %I:%M %p", tz = "US/Eastern")
-  ) +
-  scale_color_manual(
-    values = c("votes_governor_25_dem" = "#3791FF", "votes_governor_25_rep" = "#F6573E"),
-    labels = c("votes_governor_25_dem" = "Democrat", "votes_governor_25_rep" = "Republican")
-  )
-
-# plots with intervals
-summaries |>
-  filter(outcome != "turnout", uncertainty == "conformal", lengths(covars) == 1, method == "lm", subset != '') |>
-  ggplot(aes(x = timestamp, y = estimate, color = outcome)) +
-  geom_pointrange(aes(ymin = lower, ymax = upper), position = position_dodge(width = 500)) +
-  # geom_point(position = position_dodge(width = 0.1)) +
-  # geom_line() +
-  geom_hline(
-    data = truth,
-    aes(yintercept = truth, color = outcome),
-    linetype = "dashed"
-  ) +
+  filter(lengths(covars) == 1, outcome != "turnout", method == "lm") |>
+  ggplot(aes(x = timestamp, y = estimate, color = outcome, shape = method)) +
+  geom_pointrange(aes(ymin = lower, ymax = upper), position = position_dodge(width = 800), size = 1) +
+  geom_segment(aes(y = lower, yend = upper, x = timestamp), data = out, color = "black", inherit.aes = FALSE) +
   facet_wrap(~vote_mode) +
   labs(
     x = "Time (EST)",
     y = "Votes",
-    color = "Party"
+    color = ""
   ) +
-  theme_bw() +
+  theme_bw(base_size = 14) +
   scale_y_continuous(
     labels = scales::label_comma(),
     n.breaks = 7
@@ -188,6 +122,65 @@ summaries |>
     labels = scales::label_date(format = "%m-%d \n %I:%M %p", tz = "US/Eastern")
   ) +
   scale_color_manual(
-    values = c("votes_governor_25_dem" = "#3791FF", "votes_governor_25_rep" = "#F6573E"),
-    labels = c("votes_governor_25_dem" = "Democrat", "votes_governor_25_rep" = "Republican")
+    values = c("votes_governor_25_dem" = "#005599", "votes_governor_25_rep" = "#ce0008", "turnout" = "grey50"),
+    labels = c("votes_governor_25_dem" = "Democrat", "votes_governor_25_rep" = "Republican", "turnout" = "Turnout")
   )
+
+
+### CQR testing
+
+data_VA = merge_data(
+  # data = tar_read(data_VA_NA),
+  data = read_csv(tests[3]),
+  history = tar_read(history_VA_NA),
+  office = c("Governor", "Attorney General", "Lt Governor"),
+  impute = FALSE
+) |>
+  add_obs("votes_governor_25_dem", "votes_potus_24_dem") |> 
+  drop_na(votes_governor_25_dem, votes_potus_24_dem)
+
+data_VA = add_resid(data_VA, votes_governor_25_dem, votes_potus_24_dem)
+
+train = filter(data_VA, obs > 0.25)
+test = filter(data_VA, obs <= 0.25)
+
+fit = enightmodels::forecast_prec(
+  data = data_VA,
+  cands = c(votes_governor_25_dem, votes_governor_25_rep),
+  turn = turnout,
+  obs = obs < 0.25,
+  ctrl_form = ~ log_s(votes_potus_24_dem) + log_s(white) + log_s(col),
+  prior_turn = votes_precFinal_21,
+  prior_n_turn = 1,
+  prior_n_cands = 1,
+  prior_cands = c(votes_potus_24_dem, votes_potus_24_rep)
+  # weights = data_VA$reg24
+)
+
+forecast_summarize(fit)
+
+fit = enightmodels::lm_bayes(
+  resid ~ votes_potus_24_dem + age1 + age2 + age3 + white + black + col,
+  data = train, 
+  weights = train$reg24
+)
+
+preds = predict(fit, newdata = test)
+
+out = bind_rows(
+  train, 
+  test |> mutate(
+    estimate = preds[, "estimate"] * votes_potus_24_dem + votes_potus_24_dem,
+    lower = preds$lower * votes_potus_24_dem + votes_potus_24_dem,
+    upper = preds$upper * votes_potus_24_dem + votes_potus_24_dem
+  )
+) |> 
+  mutate(
+    estimate = coalesce(estimate, votes_governor_25_dem),
+    lower = coalesce(lower, votes_governor_25_dem),
+    upper = coalesce(upper, votes_governor_25_dem)
+  ) |> 
+    summarize(
+      across(c(estimate, lower, upper), sum),
+      .by = c(timestamp, vote_mode)
+    )
